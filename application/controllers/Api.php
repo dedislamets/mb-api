1ef119b77bbf642fd02e51405a1912961aba2d48
dedislamets <dedi.slamets@gmail.com>
2022-03-01 00:37:06 +0700

    Kapal Sewa Home

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -128,6 +128,35 @@ class Api extends RestController  {
         }
     }
 
+    public function front_kapal_sewa_get()
+    {
+        $live = $this->db->select('t_iklan.*,nama_foto,nama_foto_thumb,vessel_nama,place_build,year_build,construction')
+                ->from('t_iklan')
+                ->join('t_foto_kapal', 't_foto_kapal.clasification_no=t_iklan.clasification_no','left')
+                ->join('t_kapal', 't_kapal.clasification_no=t_iklan.clasification_no','left')
+                ->where(array('status' => 1,'active' => 1,'service' => 'Chartering'))
+                ->group_by('t_iklan.id')
+                ->order_by('date_iklan','desc')  
+                ->limit(6)           
+                ->get()
+                ->result_array();
+
+        if ($live != FALSE) {
+            $this->response([
+                'status' => true,
+                'title' => 'Kapal Terbaru',
+                'horizontal' => true,
+                'data' => $live
+            ], 200 );
+        }else{
+
+            $this->response( [
+                'status' => false,
+                'message' => 'No data were found'
+            ], 404 );
+        }
+    }
+
     
 
     public function room_byid_get()
